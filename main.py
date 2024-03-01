c354975544a1d0d61ddae00c239c5dc8517ab2a0
biancaBezerra <bianca.skills@gmail.com>
2024-03-01 11:28:03 -0300

    função de jogar novamente criada

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import random
 
-numero_secreto = random.randint(1,100)
-
 def quem_comeca(nome_jogador):
     return random.choice([nome_jogador, "COMPUTADOR"])
 
@@ -24,41 +22,49 @@ def verificar_palpite(palpite, numero_secreto):
         print("O número é menor que", palpite)
     return False
 
-palpites_jogador = []
-palpites_computador = []
-
-print("Bem vindo ao Jogo ADIVINHE O NÚMERO!")
-nome_jogador = input("Por favor, digite seu nome: ").upper()
-
-comeca_primeiro = quem_comeca(nome_jogador)
-print(f"Sorteado para começa primeiro: {comeca_primeiro}")
 
 while True:
-    if comeca_primeiro == nome_jogador:
-        print(f"\n----------------------- Sua Vez {nome_jogador} -----------------------")
-        palpite = fazer_palpite_humano(palpites_jogador)
-        if verificar_palpite(palpite, numero_secreto):
-            print(f"Parabéns {nome_jogador}, você acertou! O número secreto era: {numero_secreto}")
-            break
-    else:
-        print("\n----------------------- Vez do Computador -----------------------")
-        palpite = fazer_palpite_computador(palpites_computador)
-        if verificar_palpite(palpite, numero_secreto):
-            print(f"A máquina venceu o jogo. O número secreto era {numero_secreto}")
-            break
+  numero_secreto = random.randint(1,100)
+  palpites_jogador = []
+  palpites_computador = []
 
-    if comeca_primeiro != nome_jogador:
-        print(f"\n----------------------- Sua Vez {nome_jogador} -----------------------")
-        palpite = fazer_palpite_humano(palpites_jogador)
-        if verificar_palpite(palpite, numero_secreto):
-            print(f"Parabéns {nome_jogador}, você acertou! O número secreto era: {numero_secreto}")
-            break
-    else:
-        print("----------------------- Vez do Computador -----------------------")
-        palpite = fazer_palpite_computador(palpites_computador)
-        if verificar_palpite(palpite, numero_secreto):
-            print(f"A máquina venceu o jogo. O número secreto era {numero_secreto}")
-            break
-        
-print(f"Palpites do Jogador {nome_jogador}: {palpites_jogador}")
-print(f"Palpites do Computador: {palpites_computador}")
+  print("Bem vindo ao Jogo ADIVINHE O NÚMERO!")
+  nome_jogador = input("\nPor favor, digite seu nome: ").upper()
+
+  comeca_primeiro = quem_comeca(nome_jogador)
+  print(f"\nSorteado para começa primeiro: {comeca_primeiro}")
+
+  while True:
+      if comeca_primeiro == nome_jogador:
+          print(f"\n----------------------- Sua Vez {nome_jogador} -----------------------")
+          palpite = fazer_palpite_humano(palpites_jogador)
+          if verificar_palpite(palpite, numero_secreto):
+              print(f"\nParabéns {nome_jogador}, você acertou! O número secreto era: {numero_secreto}")
+              break
+      else:
+          print("\n----------------------- Vez do Computador -----------------------")
+          palpite = fazer_palpite_computador(palpites_computador)
+          if verificar_palpite(palpite, numero_secreto):
+              print(f"\nA máquina venceu o jogo. O número secreto era: {numero_secreto}")
+              break
+
+      if comeca_primeiro != nome_jogador:
+          print(f"\n----------------------- Sua Vez {nome_jogador} -----------------------")
+          palpite = fazer_palpite_humano(palpites_jogador)
+          if verificar_palpite(palpite, numero_secreto):
+              print(f"\nParabéns {nome_jogador}, você acertou! O número secreto era: {numero_secreto}")
+              break
+      else:
+          print("----------------------- Vez do Computador -----------------------")
+          palpite = fazer_palpite_computador(palpites_computador)
+          if verificar_palpite(palpite, numero_secreto):
+              print(f"\nA máquina venceu o jogo. O número secreto era: {numero_secreto}")
+              break
+
+  print(f"\nPalpites do Jogador {nome_jogador}: {palpites_jogador}")
+  print(f"Palpites do Computador: {palpites_computador}")
+
+  jogar_novamente = input("\nDeseja jogar novamente? (s/n): ").lower()
+  if jogar_novamente != 's':
+    print("\nObrigado por jogar! Até a próxima.")
+    break
