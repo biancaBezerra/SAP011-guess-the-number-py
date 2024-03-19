f9d27cf8671adf157d6be92057408baaefff9ca1
biancaBezerra <bianca.skills@gmail.com>
2024-03-19 10:45:21 -0300

    master: test - inserção do teste verificar_palpite

--- main_test.py
+++ main_test.py
@@ -1,4 +1,4 @@
-from main import fazer_palpite_computador, quem_comeca, fazer_palpite_humano
+from main import fazer_palpite_computador, quem_comeca, fazer_palpite_humano, fazer_palpite, verificar_palpite
 
 def test_quem_comeca(monkeypatch):
     # Simulando entrada do jogador
@@ -24,4 +24,30 @@ def test_fazer_palpite_computador():
     
     # Verifica se o palpite foi adicionado à lista de palpites do computador
     assert palpite in palpites_computador
+
+
+def test_fazer_palpite(monkeypatch):
+    palpites_jogador = []
+    palpites_computador = []
+
+    # Teste para o jogador humano
+    entrada = "10"
+    monkeypatch.setattr('builtins.input', lambda _: entrada)
+    assert fazer_palpite("HUMANO", palpites_jogador, palpites_computador) == int(entrada)
+    assert palpites_jogador == [int(entrada)]
+
+    # Teste para o jogador computador
+    palpite_computador = fazer_palpite("COMPUTADOR", palpites_jogador, palpites_computador)
+    assert 1 <= palpite_computador <= 100
+    assert palpite_computador in palpites_computador
+
+def test_verificar_palpite():
+    #caso o palpite seja igual ao numero secreto retorna true
+    assert verificar_palpite(40, 40) == True
+
+    #caso o palpite seja menor que o numero secreto
+    assert verificar_palpite(25, 40) == False
+
+    #caso o palpite seja maior que o numero secreto
+    assert verificar_palpite(50,40) == False
 
