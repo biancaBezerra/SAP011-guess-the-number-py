80d166e57fb971effa9d011c39136136119781ae
biancaBezerra <bianca.skills@gmail.com>
2024-03-25 18:48:46 -0300

    main: test - atualização dos testes, exclusão de etstes desnecessários

--- main_test.py
+++ main_test.py
@@ -1,8 +1,6 @@
-import main
+from main import palpite_computador, quem_comeca, palpite_humano, verificar_palpite
 
-from main import palpite_computador, quem_comeca, palpite_humano, fazer_palpite, verificar_palpite, reiniciar_jogo
-
-def test_quem_comeca(monkeypatch):
+def test_quem_comeca(monkeypatch): ##esse teste aqui é opcional, pois o choice ja foi testado
     # Simulando entrada do jogador
     nome_jogador = "Jogador"  # Simulando entrada do jogador
 
@@ -15,35 +13,16 @@ def test_palpite_humano(monkeypatch):
     palpites_jogador = []
     entrada = 10
     monkeypatch.setattr('builtins.input', lambda _: entrada)
-    assert 1 <= entrada <= 100
-    assert palpite_humano(palpites_jogador) == int (entrada)
+    assert palpite_humano(palpites_jogador) == entrada
+    assert entrada in palpites_jogador
 
 def test_palpite_computador():
     palpites_computador = []
     palpite = palpite_computador(palpites_computador)
-    
     # Verifica se o palpite está dentro do intervalo de 1 a 100
     assert 1 <= palpite <= 100
-    
     # Verifica se o palpite foi adicionado à lista de palpites do computador
     assert palpite in palpites_computador
-
-
-def test_fazer_palpite(monkeypatch):
-    palpites_jogador = []
-    palpites_computador = []
-
-    # Teste para o jogador humano
-    entrada = "10"
-    monkeypatch.setattr('builtins.input', lambda _: entrada)
-    assert fazer_palpite("HUMANO", palpites_jogador, palpites_computador) == int(entrada)
-    assert palpites_jogador == [int(entrada)]
-
-    # Teste para o jogador computador
-    palpite_computador = fazer_palpite("COMPUTADOR", palpites_jogador, palpites_computador)
-    assert 1 <= palpite_computador <= 100
-    assert palpite_computador in palpites_computador
-
 
 def test_verificar_palpite():
     #caso o palpite seja igual ao numero secreto retorna true
@@ -54,11 +33,3 @@ def test_verificar_palpite():
 
     #caso o palpite seja maior que o numero secreto
     assert verificar_palpite(50,40) == False
-
-def test_reiniciar_jogo_sim(monkeypatch):
-    #caso o jogador queira jogar novamente
-    jogar_novamente = "s"
-    monkeypatch.setattr('builtins.input', lambda _: jogar_novamente)
-    assert reiniciar_jogo() == True
-
-
